f3fc04a738fe6469b88d28f5a58070f4f7677e10
frank-west-tedesco <166546868+frank-west-tedesco@users.noreply.github.com>
2024-09-24 21:22:33 -0300

    Add files via upload
    just my part

---
+++ TPN2.js
@@ -0,0 +1,12 @@
+//ramiro
+
+//luz
+
+//yo de nuevo
+let edadB  = prompt("ingresa tu edad") ;
+v = (edadB >=18)? console.log ("asi que tu edad es " + edadB + " por lo tanto eres mayor de edad"  ) : ("asi que tu edad es "+ edadB +" por lo tanto no eres mayor de edad y no vas a poder comprarte el gears of war E-day" );
+
+//Uh, here I go, here I go, here I go again
+let Password_of_the_knights_who_say________Nii  = prompt("escribe tu contraseña") ;
+let superduperhyperultramegaching_secret_Password = prompt ("ahora escribela de nuevo");
+versh = (Password_of_the_knights_who_say________Nii == superduperhyperultramegaching_secret_Password)? console.log ("Contraseña correcta"  ) : ("contraseña incorrecta prueba con otra que no sea " + superduperhyperultramegaching_secret_Password );
